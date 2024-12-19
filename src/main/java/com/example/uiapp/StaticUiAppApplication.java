47e85069066473ae24b83843e712f690c76b3471
Narendra Reddy <narendrareddy00@gmail.com>
2024-12-19 09:30:31 -0600

    message

---
+++ src/main/java/com/example/uiapp/StaticUiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.uiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticUiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticUiAppApplication.class, args);
+	}
+
+}
